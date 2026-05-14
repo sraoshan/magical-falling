c852486e57e8c534cd70d216acc4393b59bbe710
sraoshan <felixfarias.pedro@gmail.com>
2026-05-14 18:55:11 -0300

    Simple version

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,3 @@
-import { MagicalLandingEngine }
-  from "./engine.js";
-
-const TARGET_SCENE_NAME =
-  "Landing Page";
-
-let engine = null;
-
-Hooks.on("canvasReady", async () => {
-
-  if (
-    canvas.scene?.name !== TARGET_SCENE_NAME
-  ) return;
-
-  engine = new MagicalLandingEngine();
-
-  engine.start();
-
-});
-
-Hooks.on("canvasTearDown", () => {
-
-  if (engine) {
-
-    engine.stop();
-
-    engine = null;
-
-  }
-
+Hooks.once("ready", () => {
+  console.log("MAGICAL FALLING OK");
 });
